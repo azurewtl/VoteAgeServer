353eceafae4729067180a3db87b640de56dba7de
Apple <apple@AppledeiMac.local>
2014-11-28 17:08:16 +0800

    static file supported

--- VoteAgeServer/settings.py
+++ VoteAgeServer/settings.py
@@ -81,7 +81,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
-
 STATIC_URL = '/static/'
+
+if DEBUG:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static-only')
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static', 'static'),
+    )
+
+
--- VoteAgeServer/urls.py
+++ VoteAgeServer/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework import routers
 from VoteAgeApp import views
+
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -19,5 +19,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^API/', include(router.urls)),
     url(r'^test/', views.test),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) 
+urlpatterns += staticfiles_urlpatterns()
+
 
